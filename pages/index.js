3989a3e9dce735b71e8761817bcab89daabb3abc
Ayrton Cardoso <ayrtoncardoso80@gmail.com>
2025-03-13 20:30:12 +0000

    modificando a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
-function Home () {
-    return <h1>Passos iniciais do curso.dev</h1>
+function Home() {
+    return (
+        <div style={{ textAlign: 'center', marginTop: '50px' }}>
+            <h1>Explorando Novos Horizontes no curso.dev</h1>
+            <p>Seja bem-vindo à sua jornada de aprendizado! 🚀</p>
+            <p>Prepare-se para desafios, conquistas e muito código! 💻✨</p>
+        </div>
+    );
 }
 
 export default Home;
